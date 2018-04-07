920d772536f582abb01effd2c8ed1d74e4937992
Gabriel Miller <millergabe10@gmail.com>
2018-04-07 17:04:31 -0400

    save

--- baselineC/ReactionGameBaselineC.X/ReactionGameBaselineC.c
+++ baselineC/ReactionGameBaselineC.X/ReactionGameBaselineC.c
@@ -9,5 +9,46 @@
  * Processor: PIC12F509
  * Compiler: XC8 v1.45 Free
  * 
- * Description: 
+ * Description: Simple Button Game
  */
+
+#include <xc.h>
+#include <stdint.h>
+
+#pragma config MCLRE = OFF, CP = OFF, WDT = OFF, OSC = IntRC
+
+#define _XTAL_FREQ 4000000
+
+#define START GPIObits.GP2
+#define SUCCESS GPIObits.GP1
+
+#define BUTTON GPIObits.GP3
+
+#define MAXRT 200
+
+void main() {
+   uint8_t cnt_8ms;
+   
+   TRIS = 0b111001;
+   OPTION = 0b11010100;
+   
+   for (;;) {
+       GPIO = 0;
+       
+       __delay_ms(2000);
+       
+       START = 1;
+       
+       cnt_8ms = 0;
+       while (BUTTON == 1 && cnt_8ms < 1000/8) {
+           TMR0 = 0;
+           while (TMR0 < 8000/32);
+           ++cnt_8ms;
+       }
+       
+       if (cnt_8ms < MAXRT/8) 
+           SUCCESS = 1;
+       
+       __delay_ms(1000);
+   }
+}
--- baselineC/ReactionGameBaselineC.X/nbproject/Makefile-default.mk
+++ baselineC/ReactionGameBaselineC.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=ReactionGameBaselineC.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ReactionGameBaselineC.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/ReactionGameBaselineC.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/ReactionGameBaselineC.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=ReactionGameBaselineC.c
 
 
 CFLAGS=
@@ -93,7 +93,23 @@ MP_PROCESSOR_OPTION=12F509
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/ReactionGameBaselineC.p1: ReactionGameBaselineC.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ReactionGameBaselineC.p1.d 
+	@${RM} ${OBJECTDIR}/ReactionGameBaselineC.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ReactionGameBaselineC.p1  ReactionGameBaselineC.c 
+	@-${MV} ${OBJECTDIR}/ReactionGameBaselineC.d ${OBJECTDIR}/ReactionGameBaselineC.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ReactionGameBaselineC.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/ReactionGameBaselineC.p1: ReactionGameBaselineC.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ReactionGameBaselineC.p1.d 
+	@${RM} ${OBJECTDIR}/ReactionGameBaselineC.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ReactionGameBaselineC.p1  ReactionGameBaselineC.c 
+	@-${MV} ${OBJECTDIR}/ReactionGameBaselineC.d ${OBJECTDIR}/ReactionGameBaselineC.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ReactionGameBaselineC.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
