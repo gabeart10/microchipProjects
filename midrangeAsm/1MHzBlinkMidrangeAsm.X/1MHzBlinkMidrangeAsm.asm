d699d81fe34690a3b8d7ada15818fd2ab786dd9f
Gabriel Miller <millergabe10@gmail.com>
2018-04-16 08:38:55 -0400

    save

---
+++ midrangeAsm/1MHzBlinkMidrangeAsm.X/1MHzBlinkMidrangeAsm.asm
@@ -0,0 +1,171 @@
+;*******************************************************************************
+;                                                                              *
+;    Microchip licenses this software to you solely for use with Microchip     *
+;    products. The software is owned by Microchip and/or its licensors, and is *
+;    protected under applicable copyright laws.  All rights reserved.          *
+;                                                                              *
+;    This software and any accompanying information is for suggestion only.    *
+;    It shall not be deemed to modify Microchip?s standard warranty for its    *
+;    products.  It is your responsibility to ensure that this software meets   *
+;    your requirements.                                                        *
+;                                                                              *
+;    SOFTWARE IS PROVIDED "AS IS".  MICROCHIP AND ITS LICENSORS EXPRESSLY      *
+;    DISCLAIM ANY WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING  *
+;    BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS    *
+;    FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL          *
+;    MICROCHIP OR ITS LICENSORS BE LIABLE FOR ANY INCIDENTAL, SPECIAL,         *
+;    INDIRECT OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, HARM TO     *
+;    YOUR EQUIPMENT, COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR    *
+;    SERVICES, ANY CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY   *
+;    DEFENSE THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER      *
+;    SIMILAR COSTS.                                                            *
+;                                                                              *
+;    To the fullest extend allowed by law, Microchip and its licensors         *
+;    liability shall not exceed the amount of fee, if any, that you have paid  *
+;    directly to Microchip to use this software.                               *
+;                                                                              *
+;    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF    *
+;    THESE TERMS.                                                              *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Filename: 1MHzBlinkMidrangeAsm.asm                                                                 *
+;    Date: 4/12/18                                                                    *
+;    File Version: 0.1                                                             *
+;    Author: Gabriel Miller                                                                   *
+;    Company:                                                                  *
+;    Description:                                                              *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Notes: In the MPLAB X Help, refer to the MPASM Assembler documentation    *
+;    for information on assembly instructions.                                 *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Known Issues: This template is designed for relocatable code.  As such,   *
+;    build errors such as "Directive only allowed when generating an object    *
+;    file" will result when the 'Build in Absolute Mode' checkbox is selected  *
+;    in the project properties.  Designing code in absolute mode is            *
+;    antiquated - use relocatable mode.                                        *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Revision History:                                                         *
+;                                                                              *
+;*******************************************************************************
+
+
+
+;*******************************************************************************
+; Processor Inclusion
+;*******************************************************************************
+
+list p=12F629
+#include <p12F629.inc>
+    
+errorlevel -302
+errorlevel -312
+    
+;*******************************************************************************
+; Configuration Word Setup
+;*******************************************************************************
+
+__CONFIG _MCLRE_ON & _CP_OFF & _CPD_OFF & _BODEN_OFF & _WDT_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT
+
+;*******************************************************************************
+; Variable Definitions
+;*******************************************************************************
+    
+    constant nLED=2
+    
+CONTEXT UDATA_SHR
+cs_W res 1
+cs_STATUS res 1
+
+GENVAR UDATA_SHR
+sGPIO res 1
+cnt_t0 res 1
+cnt_10ms res 1
+ 
+;*******************************************************************************
+; Reset Vector
+;*******************************************************************************
+
+RES_VECT  CODE    0x0000            ; processor reset vector
+pagesel START
+GOTO    START                   ; go to beginning of program
+  
+;*******************************************************************************
+; ISR
+;*******************************************************************************
+  
+ISR CODE 0x0004
+    movwf cs_W
+    movf STATUS,w
+    movwf cs_STATUS
+    
+    movlw .256-.250+.3
+    banksel TMR0
+    addwf TMR0,f
+    bcf INTCON, T0IF
+    
+    decfsz cnt_t0,f
+    goto irs_end
+    movlw .40
+    movwf cnt_t0
+    
+    decfsz cnt_10ms,f
+    goto irs_end
+    movlw .50
+    movwf cnt_10ms
+    
+    movf sGPIO,w
+    xorlw 1<<nLED
+    movwf sGPIO
+    
+irs_end
+    movf cs_STATUS,w
+    movwf STATUS
+    swapf cs_W,f
+    swapf cs_W,w
+    retfie
+
+;*******************************************************************************
+; MAIN PROGRAM
+;****************x***************************************************************
+
+MAIN_PROG CODE                      ; let linker place main program
+
+START
+    call 0x03FF
+    banksel OSCCAL
+    movwf OSCCAL
+    
+    banksel GPIO
+    clrf GPIO
+    clrf sGPIO
+    movlw ~(1<<nLED)
+    banksel TRISIO
+    movwf TRISIO
+    
+    movlw b'11001000'
+    banksel OPTION_REG
+    movwf OPTION_REG
+    
+    movlw .40
+    movwf cnt_t0
+    movlw .50
+    movwf cnt_10ms
+    
+    movlw 1<<GIE|1<<T0IE
+    movwf INTCON
+    
+main_loop
+    movf sGPIO,w
+    banksel GPIO
+    movwf GPIO
+    
+    goto main_loop
+
+    END
---
+++ midrangeAsm/BlinkLedMidrangeAsm.X/BlinkLedMidrangeAsm.asm
@@ -0,0 +1,129 @@
+;*******************************************************************************
+;                                                                              *
+;    Microchip licenses this software to you solely for use with Microchip     *
+;    products. The software is owned by Microchip and/or its licensors, and is *
+;    protected under applicable copyright laws.  All rights reserved.          *
+;                                                                              *
+;    This software and any accompanying information is for suggestion only.    *
+;    It shall not be deemed to modify Microchip?s standard warranty for its    *
+;    products.  It is your responsibility to ensure that this software meets   *
+;    your requirements.                                                        *
+;                                                                              *
+;    SOFTWARE IS PROVIDED "AS IS".  MICROCHIP AND ITS LICENSORS EXPRESSLY      *
+;    DISCLAIM ANY WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING  *
+;    BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS    *
+;    FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL          *
+;    MICROCHIP OR ITS LICENSORS BE LIABLE FOR ANY INCIDENTAL, SPECIAL,         *
+;    INDIRECT OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, HARM TO     *
+;    YOUR EQUIPMENT, COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR    *
+;    SERVICES, ANY CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY   *
+;    DEFENSE THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER      *
+;    SIMILAR COSTS.                                                            *
+;                                                                              *
+;    To the fullest extend allowed by law, Microchip and its licensors         *
+;    liability shall not exceed the amount of fee, if any, that you have paid  *
+;    directly to Microchip to use this software.                               *
+;                                                                              *
+;    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF    *
+;    THESE TERMS.                                                              *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Filename: BlinkLedMidrangeAsm.asm                                                                 *
+;    Date: 4/12/18                                                                    *
+;    File Version: 0.1                                                             *
+;    Author: Gabriel Miller                                                                   *
+;    Company:                                                                  *
+;    Description:                                                              *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Notes: In the MPLAB X Help, refer to the MPASM Assembler documentation    *
+;    for information on assembly instructions.                                 *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Known Issues: This template is designed for relocatable code.  As such,   *
+;    build errors such as "Directive only allowed when generating an object    *
+;    file" will result when the 'Build in Absolute Mode' checkbox is selected  *
+;    in the project properties.  Designing code in absolute mode is            *
+;    antiquated - use relocatable mode.                                        *
+;                                                                              *
+;*******************************************************************************
+;                                                                              *
+;    Revision History:                                                         *
+;                                                                              *
+;*******************************************************************************
+
+
+
+;*******************************************************************************
+; Processor Inclusion
+;*******************************************************************************
+
+list p=12F629
+#include <p12F629.inc>
+    
+errorlevel -302
+    
+;*************************************************+*****************************
+; Configuration Word Setup
+;*******************************************************************************
+
+__CONFIG _MCLRE_ON & _CP_OFF & _CPD_OFF & _BODEN_OFF & _WDT_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT
+
+;*******************************************************************************
+; Variable Definitions
+;*******************************************************************************
+
+    UDATA_SHR
+sGPIO res 1
+dc1 res 1
+dc2 res 1
+ 
+    
+;*******************************************************************************
+; Reset Vector + RC Cal
+;*******************************************************************************
+
+RES_VECT  CODE    0x0000            ; processor reset vector
+call 0x03FF
+banksel OSCCAL
+movwf OSCCAL
+GOTO    START                   ; go to beginning of program
+
+;*******************************************************************************
+; MAIN PROGRAM
+;****************x***************************************************************
+
+MAIN_PROG CODE                      ; let linker place main program
+
+START
+
+    movlw ~(1<<GP1)
+    banksel TRISIO
+    movwf TRISIO
+    
+    clrf sGPIO  
+main_loop
+    movf sGPIO,w
+    xorlw 1<<GP1
+    movwf sGPIO
+    banksel GPIO
+    movwf GPIO
+    
+    movlw .244
+    movwf dc2
+    clrf dc1
+dly1 nop
+    decfsz dc1,f
+    goto dly1
+dly2 nop
+    decfsz dc1,f
+    goto dly2
+    decfsz dc2,f
+    goto dly1
+    
+    goto main_loop
+ 
+    END
